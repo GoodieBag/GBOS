3f9046f5db3aa7169341379d40a4335e59d8ae04
Koushik <koushik.n1994@gmail.com>
2018-01-27 00:08:57 +0530

    Updated print_string.asm

--- print_string.asm
+++ print_string.asm
@@ -2,11 +2,7 @@
 PRINT_MESSAGE:
 pusha
 call PRINT_STRING
-
-;mov bx, MY_MESSAGE
-;call PRINT_MESSAGE
-
-jmp EXIT
+ret
 
 PRINT_STRING:
 mov al, [bx]
@@ -17,13 +13,6 @@ mov ah, 0x0e
 int 0x10
 jmp PRINT_STRING
 
-
-MY_MESSAGE :
-db 'Hello Kai', 0
-
 EXIT:
 popa
 ret
-
-;times 510-($-$$) db 0
-;dw 0xaa55
