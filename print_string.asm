05528108aa56816f9b1e07fb5a9e8d435046aff7
Koushik <koushik.n1994@gmail.com>
2018-01-27 00:48:26 +0530

    Updated print_string.asm

--- print_string.asm
+++ print_string.asm
@@ -1,18 +1,18 @@
 
 PRINT_MESSAGE:
-pusha
+pusha  				;push all register values to the stack
 call PRINT_STRING
-ret
+popa 				;Restore orignal register values
+ret 				;Returns to the main function
 
 PRINT_STRING:
-mov al, [bx]
-inc bx
-cmp al, 0x0
+mov al, [bx]		;bx has the address of the input string lable(i.e address of the 1st character in the string)
+inc bx				;point to the next character
+cmp al, 0x0 		;Looking for the exit null character in the string
 jz EXIT
 mov ah, 0x0e
 int 0x10
 jmp PRINT_STRING
 
 EXIT:
-popa
-ret
+ret 				;Returns to the next instruction after the "call PRINT_STRING"
