a3f94c78f8bcff144b28af3b9facc100151e0dde
delavega <preetham.kulai21@gmail.com>
2018-01-28 22:51:58 +0530

    incrementing bx immediately after int 0x10 to avoid one extra increment of bx

--- print_string.asm
+++ print_string.asm
@@ -7,11 +7,11 @@ ret 				;Returns to the main function
 
 PRINT_STRING:
 mov al, [bx]		;bx has the address of the input string lable(i.e address of the 1st character in the string)
-inc bx				;point to the next character
 cmp al, 0x0 		;Looking for the exit null character in the string
 jz EXIT
 mov ah, 0x0e
 int 0x10
+inc bx				;point to the next character
 jmp PRINT_STRING
 
 EXIT:
